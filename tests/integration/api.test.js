0ff29c74d457f9f1356da266a247267f93b96027
Osman Kalache <osmane.kalache@cevital.com>
2019-01-24 15:32:03 +0100

    first api test

--- tests/integration/api.test.js
+++ tests/integration/api.test.js
@@ -2,23 +2,25 @@ process.env.NODE_ENV = "test";
 
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-let app = require("../../index");
-let should = chai.should();
-
-const { sequelize } = require("../../models");
+const app = require("../../index");
+const should = chai.should();
 
 chai.use(chaiHttp);
 
 describe("/api", () => {
-
-  it("GET /api/ should 403", done => {
-    chai
-      .request(app)
-      .get("/api")
-      .end((err, res) => {
-        res.should.have.status(403);
-        done();
-      });
+  describe("/api/auth", () => {
+    it("should 401 with bad credencials", done => {
+      chai
+        .request(app)
+        .post("/api/auth")
+        .send({
+          username: "someuser",
+          password: "somepass"
+        })
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        });
+    });
   });
 });
-
